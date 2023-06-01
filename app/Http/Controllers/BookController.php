70567ac68925942e11206ecafe0a315fcf7fe5e9
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2023-06-01 12:55:27 +0500

    books get and create functionality added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    use ApiResponse;
+
+    public function index()
+    {
+        return Book::whereUserId(1)->get();
+    }
+
+
+    public function create(Request $request)
+    {
+        $user_id = 1;
+
+        $book = new Book;
+        $book->user_id = $user_id;
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->genre = $request->genre;
+        $book->image = $request->image;
+        $book->save();
+
+        return $this->success($book->toArray(), 'Book has been added to library');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+/// ----- LOGIN ----- \\\
+Route::post('auth/login', [LoginController::class, 'login']);
+Route::post('auth/register', [LoginController::class, 'register']);
+
+
+Route::group([ 'middleware' => 'auth:sanctum' ], function($router) {
+    
+    $router->get('books', [BookController::class, 'index']);
+    $router->post('books/create', [BookController::class, 'create']);
 });
+
